4ec8171aa2572fd9c7e2f91c69b9aa6d3addc83e
kavinsharma <kavinsharma457@gmail.com>
2024-11-09 06:50:14 +0530

    feat(packages) : added the necessary packages and modification in app and index.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const config = require("./config");
-const { connectToReqDatabase } = require("./core/scripts/db.connection");
-
 const routes = require("./app/routes/index.routes");
-// const { setupLogger } = require("./app/middleware/logger.middleware");
 
 const app = express();
-// setupLogger(app);
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
@@ -16,8 +12,6 @@ app.use(
     origin: config.CLIENT_URL,
   }),
 );
-
-app.use(connectToReqDatabase);
 
 app.use("/api/v1", routes);
 
