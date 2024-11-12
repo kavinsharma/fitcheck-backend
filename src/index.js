36fdca1c7148aeec5df0ada77c362bfbbad0ad7d
kavinsharma <kavinsharma457@gmail.com>
2024-11-12 15:14:15 +0530

    feat(packages) : added the necessary packages and modification in app and index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const app = require("./app.js");
 const config = require("./config");
+const { connectToDb } = require("./core/scripts/db.connection.js");
 const PORT = config.PORT ?? 8080;
 
 app.listen(PORT, async () => {
-  console.log(`🚀 ~ app.listen ~ PORT: ${PORT}`);
+  console.log("🚀 ~ app.listen ~ PORT:", `${PORT}`);
+  const isConnected = await connectToDb();
+  if (!isConnected) {
+    console.error("Process has been terminated due to db connection failure!!");
+    process.exit(0);
+  }
 });
